2d9b84c7129a91afa579e4268ab5d2c1bdb369e6
adrielecolossi <adriele.colossi4@gmail.com>
2022-08-11 18:28:21 -0300

    Update apiClient.js

--- src/apiClient.js
+++ src/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-axios.defaults.baseURL = 'http://localhost:3001/'
+axios.defaults.baseURL = 'https://bancolibrasptb.herokuapp.com'
 
 
 axios.interceptors.response.use(
