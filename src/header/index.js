a15c04586590356db07d084ee90ea75101645ad5
adrielecolossi <adriele.colossi4@gmail.com>
2022-08-11 19:03:18 -0300

    Update index.js

--- src/header/index.js
+++ src/header/index.js
@@ -10,7 +10,6 @@ function HeaderOne(props) {
 function removeStorage(){
   localStorage.removeItem('tokenLibrasPTB');
   localStorage.removeItem('user');
-  window.location.href= "http://localhost:3000/login"
 }
   if (props.logged === true) {
     return (
